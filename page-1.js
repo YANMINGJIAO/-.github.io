1221f6e51922e0974bfa5cec2a14d02775077907
綾鷹っぽい？ <100072107+YANMINGJIAO@users.noreply.github.com>
2024-07-03 13:24:02 +0900

    Update page-1.js

--- page-1.js
+++ page-1.js
@@ -1,28 +1,27 @@
 const app = new Vue({
   el: "#app",
   data: {
-    message: "写真を撮影しました", //メッセージを表示
+    message: "写真を撮影しました"
     seen: false
   },
   methods: {
     move(){
-	   const se = new Audio("camera.mp3") //効果音を読み込む
+	   const se = new Audio("camera.mp3")
 		se.play() //再生
       this.seen = true
 		 setTimeout(() => {
-	     this.message = "写真を送信中..." //0.8秒経過までメッセージを表示する
+	     this.message = "写真を送信中..."
       }, 800)
 		setTimeout(() => {
-	     this.message = "IPアドレス取得中..." //1.6秒経過までメッセージを表示する
-      }, 1600)
+	     this.message = "IPアドレス取得中..."
 		setTimeout(() => {
-	     this.message = "IPアドレスから住所特定中..." //2.4秒経過までメッセージを表示する
+	     this.message = "IPアドレスから住所特定中..."
       }, 2400)
 		setTimeout(() => {
-	     this.message = "情報送信中..." //3.2秒経過までメッセージを表示する
+	     this.message = "情報送信中..."
       }, 3200)
       setTimeout(() => {
-	     location.href = "page-2.html" //4.0秒経過でpage-2.htmlに移動
+	     location.href = "page-2.html"
       }, 4000)
 	 }
   }
